aff314ede5af0a79ff98e3fc1a8c4fe7c39b2514
Chris Henry <cdhenry@gmail.com>
2018-05-16 20:25:52 +0000

    automatically backed up by learn

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,6 +2,16 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
-
+export default function manageRestaurants(state, action) {restaurants: []}, action) {
+  switch (action.type) {
+    case 'ADD_RESTAURANT':
+      id++;
+      const restaurant = Object.assign({}, action.restaurant, { id: cuidFn() });
+      return { restaurants: state.restaurants.concat(restaurant) }
+    case 'DELETE_RESTAURANT':
+      const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id);
+      return  { restaurants }
+    default:
+      return state;
+  }
 }
